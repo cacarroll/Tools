93e7f114f856f192c0ee1368a3d9a4707a8876e2
Chad Carroll <ccarroll@microsoft.com>
2023-06-02 19:10:39 -0400

    a lot of updates

--- AzureFilesToBlobCopy.ps1
+++ AzureFilesToBlobCopy.ps1
@@ -1,44 +1,66 @@
 param (
-    $SourceSubscriptionName,
-    $SourceStorageAccountName = 'files123',
-    $SourceResourceGroupName = 'D-Sub-HUB-001',
-
-    $DestinationSubscriptionName,
-    $DestinationStorageAccountName = 'blob123',
-    $DestinationResourceGroupName = 'D-Sub-HUB-001'
+    $Release = '2Q23'
 )
+
+function Set-AzSubscription {
+    param (
+        [Parameter(Mandatory)]
+        [string]$SubscriptionId
+    )
+
+    try {
+        az account set --subscription $SubscriptionId
+        Write-Output "Successfully set subscription to: $SubscriptionId"
+    }
+    catch {
+        Write-Error "Failed to set subscription to: $SubscriptionId. Error: $($_.Exception.Message)"
+        throw
+    }
+}
+
+
 
 function Map-Drive {
     [CmdletBinding()]
     param (
-        $ResourceGroupName,
-        $StorageAccountName,
-        $ShareName,
-        $drive_letter = "Q"
+        [Parameter(Mandatory)]
+        [string]$ResourceGroupName,
+        [Parameter(Mandatory)]
+        [string]$StorageAccountName,
+        [Parameter(Mandatory)]
+        [string]$ShareName,
+        [string]$DriveLetter
     )
-
-    # Get the storage account key
-    $StorageAccountKey = $(az storage account keys list -g $ResourceGroupName -n $StorageAccountName --query "[0].value" --output tsv)
     
-    $fileShareEndpoint = $((az storage account show --name $StorageAccountName --query "primaryEndpoints.file" --output tsv) -split '/')[2]
-    $unc_path = "\\$fileShareEndpoint\$ShareName"
+    try {
+        Write-Verbose "Attempting connection to storage account: $StorageAccountName and RG: $ResourceGroupName"
 
-    if (Get-PSdrive -Name $drive_letter -ErrorAction SilentlyContinue) {
-        Remove-Psdrive -Name $drive_letter
+        $StorageAccountKey = $(az storage account keys list -g $ResourceGroupName -n $StorageAccountName --query "[0].value" --output tsv)
+
+        $FileShareEndpoint = $(az storage account show --name $StorageAccountName --query "primaryEndpoints.file" --output tsv).Split('/')[2]
+        $UNCPath = "\\$FileShareEndpoint\$ShareName"
+
+        if (Get-PSDrive -Name $DriveLetter -ErrorAction SilentlyContinue) {
+            Remove-PSDrive -Name $DriveLetter -Force
+        }
+
+        $ConnectTestResult = Test-NetConnection -ComputerName $FileShareEndpoint -Port 445
+        if ($ConnectTestResult.TcpTestSucceeded) {
+            # Save the password so the drive will persist on reboot
+            cmd.exe /C "cmdkey /add:`"$FileShareEndpoint`" /user:`"localhost\$StorageAccountName`" /pass:`"$StorageAccountKey`""
+            # Mount the drive
+            New-PSDrive -Name $DriveLetter -PSProvider FileSystem -Root $UNCPath -Persist -Scope Global
+        }
+        else {
+            throw "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+        }
     }
-   
-    $connectTestResult = Test-NetConnection -ComputerName $fileShareEndPoint -Port 445
-    if ($connectTestResult.TcpTestSucceeded) {
-        # Save the password so the drive will persist on reboot
-        cmd.exe /C "cmdkey /add:`"$fileShareEndPoint`" /user:`"localhost\$StorageAccountName`" /pass:`"$StorageAccountKey`""
-        # Mount the drive
-        New-PSDrive -Name Q -PSProvider FileSystem -Root $unc_path -Persist -scope Global
-    }
-    else {
-        Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-        Throw
+    catch {
+        Write-Error "Error occurred while mapping drive: $_"
+        throw
     }
 }
+
 
 function Get-FileShareSASUri {
     [CmdletBinding()]
@@ -46,21 +68,26 @@ function Get-FileShareSASUri {
         $StorageAccountName,
         $ShareName
     )
-
-    $expiry = (Get-Date).AddDays(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-
-    $storageKey = $(az storage account keys list --account-name $StorageAccountName --query "[?keyName=='key1'].value" --output tsv)
     
-    $sasToken = $(az storage share generate-sas -n $ShareName --account-name $StorageAccountName --account-key $storageKey --https-only --permissions dlrw --expiry $expiry -o tsv)
+    try {
+        $Expiry = (Get-Date).AddDays(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
 
-    # Get file share endpoint URL
-    $fileShareEndpoint = $(az storage account show --name $StorageAccountName --query "primaryEndpoints.file" --output tsv)
+        $StorageKey = $(az storage account keys list --account-name $StorageAccountName --query "[?keyName=='key1'].value" --output tsv)
+        
+        $SasToken = $(az storage share generate-sas -n $ShareName --account-name $StorageAccountName --account-key $StorageKey --https-only --permissions dlrw --expiry $Expiry -o tsv)
 
-    # Append file share name to endpoint URL
-    $fileShareSasUri = "$fileShareEndpoint$shareName/*?$sasToken"
+        $FileShareEndpoint = $(az storage account show --name $StorageAccountName --query "primaryEndpoints.file" --output tsv)
 
-    return $fileShareSasUri
+        $FileShareSasUri = "$FileShareEndpoint$ShareName/?$SasToken"
+
+        return $FileShareSasUri
+    }
+    catch {
+        Write-Error "Error occurred while generating File Share SAS URI: $_"
+        throw
+    }
 }
+
 
 function Get-BlobContainerSASUri {
     [CmdletBinding()]
@@ -69,37 +96,47 @@ function Get-BlobContainerSASUri {
         $ContainerName
     )
 
-    $expiry = (Get-Date).AddDays(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+    $Expiry = (Get-Date).AddDays(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
 
-    $storageKey = $(az storage account keys list --account-name $StorageAccountName --query "[?keyName=='key1'].value" --output tsv)
+    $StorageKey = $(az storage account keys list --account-name $StorageAccountName --query "[?keyName=='key1'].value" --output tsv)
     
-    $sasToken = $(az storage container generate-sas --account-name $StorageAccountName --account-key $storageKey --name $containerName --permissions racwdl --expiry $expiry --output tsv)
+    $SasToken = $(az storage container generate-sas --account-name $StorageAccountName --account-key $StorageKey --name $ContainerName --permissions racwdl --expiry $Expiry --output tsv)
 
-    # Get file share endpoint URL
-    $blobEndpoint = $(az storage account show --name $StorageAccountName --query "primaryEndpoints.blob" --output tsv)
+    $BlobEndpoint = $(az storage account show --name $StorageAccountName --query "primaryEndpoints.blob" --output tsv)
 
-    # Append file share name to endpoint URL
-    $blobContainerSasUri = "$blobEndpoint$shareName/?$sasToken"
+    $BlobContainerSasUri = "$BlobEndpoint$ContainerName/?$SasToken"
 
-    return $blobContainerSasUri
+    return $BlobContainerSasUri
 }
 
-Function Get-EmptyFolders {
-    [CmdletBinding()]
+function Get-EmptyFolder {
     param (
-        $Path
+        [Parameter(Mandatory=$true)]
+        [string]$Path
     )
 
-    $emptyFolders = @()
-    Get-ChildItem -Path $path -Directory -Recurse | ForEach-Object {
-        if ((Get-ChildItem -path $($_.Fullname) -Recurse).count -eq 0) {
-            $emptyFolders += $($_.Fullname).substring(3).replace('\', '/')
-        }
+    $folder = [System.IO.DirectoryInfo]::new($Path)
+
+    if ($folder.GetFiles("*", [System.IO.SearchOption]::TopDirectoryOnly).Count -eq 0 -and
+        $folder.GetDirectories().Count -eq 0) {
+        $pathSplit = $folder.FullName.Split("\")
+        $emptyFolder = ($pathSplit[3..($pathSplit.Length - 1)] -join "/")
+        return $emptyFolder
     }
-    return $emptyFolders
+    return $null
 }
 
-Function New-EmptyFile {
+function Get-NextAvailableDriveLetter {
+    $driveLetters = [char[]]('D'..'Z')
+    foreach ($letter in $driveLetters) {
+        if (!(Test-Path -Path "$letter`:\")) {
+            return $letter
+        }
+    }
+    throw "No available drive letter found."
+}
+
+function New-EmptyFile {
     [CmdletBinding()]
     param (
         $StorageAccountName,
@@ -108,56 +145,100 @@ Function New-EmptyFile {
         $ShareName
     )
 
-    $connectionString = $(az storage account show-connection-string --name $StorageAccountName --resource-group $ResourceGroupName -o tsv)
+    $ShareName = $ShareName.Replace("\","/")
+    $ConnectionString = $(az storage account show-connection-string --name $StorageAccountName --resource-group $ResourceGroupName -o tsv)
 
-    New-Item -path $env:tmp -Name empty.txt
+    New-Item -Path $env:TEMP -Name empty.txt -Force | Out-Null
 
-    foreach ($emptyFolder in $emptyFolders) {
-        Write-Output "Creating empty.txt on $StorageAccountName/$shareName/$EmptyFolder"
-        az storage blob upload-batch --destination "$shareName/$emptyFolder" --source $env:tmp --pattern "empty.txt" --account-name $StorageAccountName --connection-string $ConnectionString
+    foreach ($EmptyFolder in $EmptyFolders) {
+        $path = $EmptyFolder.replace("\","/")
+        Write-Output "Creating empty.txt on $StorageAccountName/$ShareName/$path"
+        az storage blob upload-batch --destination "$ShareName/$EmptyFolder" --source $env:TEMP --pattern "empty.txt" --account-name $StorageAccountName --connection-string $ConnectionString
     }
 
-    Remove-Item "$env:tmp\empty.txt"
+    Remove-Item "$env:TEMP\empty.txt"
 }
 
 
+Write-Host "`n************ Start copy from Azure File Share to Azure Blob Container *******************" -ForegroundColor Cyan
 
+# Convert Release Date to New Format
+$quarter, $year = $Release -split 'Q'
+$newReleaseFormat = "FY20$year\$quarter" + 'Q'
 
-# Source Subscription
-Select-AzSubscription -Name $SourceSubscriptionName
+$StorageMapping = @{
+    Source = @{
+        SubscriptionId      = '579d0760-2880-4f13-8d8f-7a2121243198'
+        StorageAccountName  = 'pipeline'
+        ResourceGroupName   = 'GD-RG'
+        FileShareName       = 'released'
+        Folder              = "Released\$Release"
+        # Folder             = 'Released\2Q23'
+    }
+    Destination = @{
+        SubscriptionId      = '16d23ae1-53e7-474f-8f46-373c815cbb01'
+        StorageAccountName  = 'releasestorage01'
+        ResourceGroupName   = 'MNR-Storage-RG'
+        BlobContainerName   = 'otm'
+        FolderMapping = @{
+            "Released\$Release" = $newReleaseFormat
+        }
+    }
+}
 
-Write-Host "`n************Start copy from Azure File Share to Azure Blob Container*******************" -ForeGroundColor Cyan
-Write-Output "Retreiving file shares from $SourceStorageAccountName"
-$SourceStorageAccountConnectionString = $(az storage account show-connection-string --name $SourceStorageAccountName --resource-group $SourceResourceGroupName -o tsv)
-$fileShares = $(az storage share list --connection-string $sourceStorageAccountConnectionString --query "[].{name:name}") | ConvertFrom-Json
+try {
+    Set-AzSubscription -SubscriptionId $StorageMapping.Source.SubscriptionId
 
-foreach ($fileShare in $fileShares.name) {
-    Write-Output "Processing fileshare $fileshare"
-    $fileShareSasUri = Get-FileShareSASUri -StorageAccountName $SourceStorageAccountName -ShareName $fileShare
+    # Map a local drive to the file share UNC
+    $DriveLetter = Get-NextAvailableDriveLetter
 
-    # Map a local drive to the file share unc
-    Map-Drive -ResourceGroupName $SourceResourceGroupName -StorageAccountName $SourceStorageAccountName -ShareName $fileshare
-  
-    # Get Empty folders on fileshare
-    $emptyFolders = Get-EmptyFolders -Path 'Q:'
+    Map-Drive -ResourceGroupName $StorageMapping.Source.ResourceGroupName -StorageAccountName $StorageMapping.Source.StorageAccountName -ShareName $StorageMapping.Source.FileShareName -DriveLetter $DriveLetter
+
+    $folder = $driveLetter + ":\" + $StorageMapping.Source.Folder
+    Write-Output "`n`nRetrieving parent folders on $folder. Please be patient..."
+    $root = [System.IO.DirectoryInfo]::new($folder)
+    $folders = $root.GetDirectories("*", [System.IO.SearchOption]::AllDirectories)
+
+    $emptyFolders = @()
+    $totalFolders = $folders.Count
+    $currentFolder = 0
+
+    foreach ($subfolder in $folders) {
+        $currentFolder++
+        Write-Output "`nScanning $($subfolder.FullName) for emptiness"
+        Write-Progress -Activity "Scanning" -Status "Processing folder $currentFolder of $totalFolders" -PercentComplete (($currentFolder/$totalFolders)*100)
+
+        $result = Get-EmptyFolder -Path $subfolder.FullName
+        if ($result) {
+            Write-Output "`nFound empty folder at $($subfolder.FullName)"
+            $emptyFolders += $result
+        }
+    }
+
+    $FileShareSasUri = Get-FileShareSASUri -StorageAccountName $StorageMapping.Source.StorageAccountName -ShareName ($StorageMapping.Source.Folder).Replace("\","/")
+
+    #### Destination Processing
+    Set-AzSubscription -SubscriptionId $StorageMapping.Destination.SubscriptionId
     
-    #Set Destination Subscription
-    Select-AzSubscription -Name $DestinationSubscriptionName
-    
-    # Mirror fileshare name to blob containers 
-    Write-Output "`nCreating blob container $fileshare on storage account $DestinationStorageAccountName"
-    $DestinationStorageAccountConnectionString = $(az storage account show-connection-string --name $DestinationStorageAccountName --resource-group $DestinationResourceGroupName -o tsv)
-    az storage container create --name $fileShare --account-name $DestinationStorageAccountName --connection-string $DestinationStorageAccountConnectionString
-    
-    #Create path and emptyfile
-    New-EmptyFile -StorageAccount $DestinationStorageAccountName -ResourceGroupName $DestinationResourceGroupName -EmptyFolders $EmptyFolders -ShareName $fileShare
+    $path = $StorageMapping.Destination.FolderMapping[$folder]
+    $DestinationBlobContainer = $StorageMapping.Destination.BlobContainerName
 
-    $blobContainerSasUri = Get-BlobContainerSasUri -StorageAccountName $DestinationStorageAccountName -containerName $fileshare
-  
-    Write-Output "`nUsing Azcopy to copy files from $fileshare on $sourceStorageAccountName to $DestinationStorageAccountName"
+    Write-Verbose "Creating blob container [$DestinationBlobContainer] on storage account $($StorageMapping.Destination.StorageAccountName)"
+    $DestinationStorageAccountConnectionString = $(az storage account show-connection-string --name $StorageMapping.Destination.StorageAccountName --resource-group $StorageMapping.Destination.ResourceGroupName -o tsv)
+    az storage container create --name $DestinationBlobContainer --account-name $StorageMapping.Destination.StorageAccountName --connection-string $DestinationStorageAccountConnectionString
+    
+    $ShareName = "$DestinationBlobContainer\$path\$newReleaseFormat"
+    New-EmptyFile -StorageAccountName $StorageMapping.Destination.StorageAccountName -ResourceGroupName $StorageMapping.Destination.ResourceGroupName -EmptyFolders $EmptyFolders -ShareName $ShareName
+
+    $BlobContainerSasUri = Get-BlobContainerSASUri -StorageAccountName $StorageMapping.Destination.StorageAccountName -ContainerName $DestinationBlobContainer
+
+    Write-Verbose "Using AzCopy to copy files from $($StorageMapping.Source.StorageAccountName)/$folder to $($StorageMapping.Destination.StorageAccountName)/$DestinationBlobContainer"
     $env:AZCOPY_CONCURRENCY_VALUE = "AUTO"
-    .\azcopy.exe cp $fileShareSasUri $blobContainerSasUri --from-to=FileBlob --s2s-preserve-access-tier=false --check-length=true --include-directory-stub=false --s2s-preserve-blob-tags=false --recursive
-    $env:AZCOPY_CONCURRENCY_VALUE = ""
+    .\azcopy.exe cp $FileShareSasUri $BlobContainerSasUri --from-to=FileBlob --s2s-preserve-access-tier=false --check-length=true --recursive        
+    # $env:AZCOPY_CONCURRENCY_VALUE = ""
 
-    Remove-Psdrive -Name 'Q'
+    Remove-PSDrive -Name $DriveLetter
+}
+catch {
+    Write-Error -Message "Error occurred: $($_.Exception.Message)"
 }
